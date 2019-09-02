d115c070cccde46287ea519e5846e7e0311931aa
Raphael B <raph.bub@gmail.com>
2019-09-02 15:05:23 +0200

    ajout js

---
+++ script.js
@@ -0,0 +1,9 @@
+setInterval(function(){
+  let dots = document.getElementById('dots').innerHTML;
+  let nbDots = dots.length;
+  if (nbDots < 3) {
+    document.getElementById('dots').innerHTML += ".";
+  } else {
+    document.getElementById('dots').innerHTML = ".";
+  }
+}, 1000);
